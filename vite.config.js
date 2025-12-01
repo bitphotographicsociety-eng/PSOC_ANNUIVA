876b0773b74697643ac131c6904b29a5bbdc69ea
bitphotographicsociety-eng <bit.photographicsociety@gmail.com>
2025-12-01 19:54:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',   // <-- IMPORTANT FIX FOR NETLIFY
   plugins: [react()],
+
   server: {
     port: 3000,
     open: true
   },
+
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
